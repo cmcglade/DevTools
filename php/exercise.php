55d7dc9ebaca59e25cb79193cd20857d00a93dbe
Christine McGlade <cmcglade825@gmail.com>
2015-11-03 23:09:21 -0500

    adding php exercise files

---
+++ php/exercise.php
@@ -0,0 +1,15 @@
+<?php  
+	$var = 'PHP Tutorial';  
+?>  
+<!DOCTYPE html>  
+<html>  
+<head>  
+<meta http-equiv="content-type" content="text/html; charset=utf-8">  
+<title><?php echo $var; ?> - DevTools</title>  
+</head>  
+<body>  
+<h1><?php echo $var; ?></h1>  
+<p>PHP, an acronym for PHP Hypertext Preprocessor, is a widely-used open source general-purpose scripting language.</p>  
+<p><a href="http://www.php.net">Go to the <?php echo $var; ?></a>.</p>  
+</body>  
+</html> 
