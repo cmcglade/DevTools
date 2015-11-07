5d71d4053840536c86a3b69d42d068c1817a9eb4
Christine McGlade <cmcglade825@gmail.com>
2015-11-07 14:22:31 -0500

    php form to generate a calendar

---
+++ php/Form_code_sample.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en>"
+<head>
+	<meta charset="UTF-8" />
+	<title>Forms</title>
+	<link rel="stylesheet" href="formstyles.css">
+</head>
+<body>
+	<form class="group" action="#">
+		<ol>
+			<li class="group">
+				<label for="countyfield">Name</label>
+				<input type="text" maxlength="10" name="name" id="namefield" />
+			</li>
+			<li class="group">
+				<label for="agefield">Age</label>
+				<input type="number" min="13" max="18" name="age" id="agefield" />
+			</li>
+			<li class="group">
+				<!-- this code will generate a calendar -->
+				<label for="datetimefield">Reservation Date</label>
+				<input type="date" min="2015-12-01" max="2015-12-31" name="date" id="date" />
+			</li>
+		</ol>
+		<button type="submit">send</button>
+	</form>
+</body>
+</html>
