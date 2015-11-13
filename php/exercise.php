6b81f4102212f525993918874416287ba026c451
cmcglade <cmcglade825@gmail.com>
2015-11-12 20:57:41 -0500

    changes made to in class demo files on November 12, re: php forms and arrays

--- php/exercise.php
+++ php/exercise.php
@@ -10,6 +10,29 @@
 <body>  
 	<h1><?php echo $var; ?></h1>  
 	<p>PHP, an acronym for PHP Hypertext Preprocessor, is a widely-used open source general-purpose scripting language.</p>  
-	<p><a href="http://www.php.net">Go to the <?php echo $var; ?></a>.</p>  
+	<p><a href="http://www.php.net">Go to the <?php echo $var; ?></a>.</p> 
+	
+	<?php
+	
+		$color = array("white", "green", "red", array("yellow", "pink"));
+		
+		echo "<p>\"The memory of that scene for me is like a frame of film forever frozen at that moment: the $color[2] carpet, the $color[1] lawn, the $color[0] house, the leaden sky. The new president and his first lady. - Richard M. Nixon\"</p>";
+		
+		//echo $color[3];
+		
+		print_r($color);
+		
+		//echo $color[1];
+		
+		echo $color[3][0];
+	
+	?>
+	
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	 
 </body>  
 </html> 
--- php/simple_form.php
+++ php/simple_form.php
@@ -7,13 +7,25 @@
 <body>  
 	<form method='POST'>  
 		<h2>Please input your name:</h2>  
-		<input type="text" name="name">  
+		<label>Name</label><input type="text" name="name"> <br> 
+		<label>Last Name</label><input type="text" name="last_name"> <br>
+		<label>Postal Code</label><input type="text" name="postal_code"> <br> 
 		<input type="submit" value="Submit Name">  
-	</form>  
-	<?php  
-	//Retrieve name from query string and store to a local variable  
-		$name = $_POST['name'];  
-		echo "<h3> Hello $name </h3>";  
-		?>  
+	</form> 
+	
+	<?php   
+		$vara = $_POST['name'];
+		$varb = $_POST['last_name'];
+		$varc = $_POST['postal_code'];
+		//strtoupper($varc);
+		//upper($var);
+		echo "<h3> Hello $vara $varb, your postal code is $varc</h3>";
+	
+	//print_r($_POST);
+	
+	?> 
+		
+		
+		 
 </body>  
 </html> 
