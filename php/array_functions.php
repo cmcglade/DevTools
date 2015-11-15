a0cc3a29445a60968344d1ea66dd2558696ea27a
Christine McGlade <cmcglade825@gmail.com>
2015-11-14 19:46:10 -0500

    advanced php examples for week 10

---
+++ php/array_functions.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang ="en">
+<head>
+	<meta charset ="utf-8">
+	<title>Iterating through an array</title>
+</head>
+<body>
+	<!--<h2>Array functions</h2>
+	
+	<?php $numbers = array(8, 23, 56, 9, 15, 12);
+		print_r($numbers);
+	?>
+	
+	<p>Count:	<?php echo count($numbers); ?><br>
+	Max value:	<?php echo max($numbers); ?><br>
+	Min value:	<?php echo min($numbers); ?><br>
+	</p> -->
+	
+	<!--<h2>Sorting values in arrays</h2>
+	<?php print_r($numbers); ?>
+	
+	<p><pre>
+	Sort: <?php echo sort($numbers); print_r($numbers); ?><br>
+	Reverse sort: <?php echo rsort($numbers); print_r($numbers); ?><br>
+	</pre>
+	Note that sort of a destructuve function: our array is now in the new order, it has been changed "in place"
+	</p>-->
+	
+	<!--<h2>Implode and Explode an array</h2>
+	
+	<p>This is really handy if you want to turn a comma seperated list for example, into array.  You will often get
+	csv files that have a lot of info in them and you may want to turthat name, lastname, address, etc into an
+	array.<br>
+	First, turning an array into a string is done using the function implode:</p>
+	
+	<?php echo implode(" + ", $numbers); ?><br>
+	
+	<?php $string = "Christine, McGlade, 243 Roxton Road, M6G3R1";
+	$new_array = explode(", ", $string);
+	print_r($new_array);
+	?><br>-->
+	
+	<!--<h2>keys and values</h2>
+	<p>will return the keys and the values only from an associative array<br>
+	looks like: array_keys() or array_values()</p>-->
+	
+	
+	
+	
+
+</body>
+</html>
