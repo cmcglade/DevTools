d2e84656e41bf0ee91764b3e987159e0f40224b6
cmcglade <cmcglade825@gmail.com>
2016-01-28 13:27:24 -0500

    twitterbot workshop files

---
+++ Twitterbot/twitterKeys.py
@@ -0,0 +1,13 @@
+#keys and access tokens for Twtterbot Makreting Rebellion
+#email: cmcglade825+marketingrebellion@gmail.com
+#password: v1rg01
+
+consumer_key = 	"B8o5M8uxhC13HhbHql7155hEr"
+consumer_secret = "W652K2yDkjk69MEop362FLWvItnkC1ZNZviaAHETKsBjAKgTLW"
+access_token = "4839349319-x7P941z7ZAAx6F514PTseE3q7T1T9yc3HuCIvU2"
+access_token_secret = "8ZWRzLzUlrjT2PhXvVtPD0Nd8Kj2QFLo3x5tAqIccGHbs"
+
+[settings]
+search_query:#yourHashtag
+# Leave empty for all languages
+tweet_language: "english"
